b21b72cdfb06f2241380b7c8e334c1e6c6e6e4b7
TueHadrup <than23@student.aau.dk>
2025-05-08 11:14:06 +0200

    MOar tests

--- ImpedanceTests/THD_interpreter.m
+++ ImpedanceTests/THD_interpreter.m
@@ -1,8 +1,8 @@
-test = readtable("zoope_7.csv");
+test = readtable("0cope_7.csv");
 
 % test = renamevars(test, "x_Freq_", "Frequency");
 
-test2 = readtable("zoope_8.csv");
+test2 = readtable("0cope_8.csv");
 % test2 = renamevars(test2, "x_Freq_", "Frequency");
 
 width  = 1200; % Width of figure
@@ -11,8 +11,8 @@ height = 400; % Height of figure
 figure('Position', [0 0 width height]);
 
 yvar = "THD";
-max(test.Var2(2:end,:))
 max(test.Var3(2:end,:))
+max(test2.Var3(2:end,:))
 
 hold on;
 plot(test, "Var1", "Var3", LineWidth=2,DisplayName="Rev1")
@@ -24,7 +24,7 @@ plot(test2,"Var1", "Var3", LineWidth=2,DisplayName="Rev2")
 % plot(test,"Frequency", "HD5")
 
 grid on
-axis([-0.005 0.005 -1 1])
+axis([-0.001 0.001 -1 1])
 xlabel("Frequency [Hz]", "FontSize", 16);
 ylabel("THD [dB]", "FontSize", 16);
 legend;
